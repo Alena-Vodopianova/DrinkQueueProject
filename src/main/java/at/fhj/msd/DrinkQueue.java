26669dbe47c305d50eef00690861336a103980c7
Afik Eschel <afikeschel@gmail.com>
2025-04-28 16:56:30 +0200

    Create empty DrinkQueue class with LinkedList<Drink>

---
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -0,0 +1,13 @@
+package at.fhj.msd;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Queue to store drinks.
+ */
+public class DrinkQueue {
+
+    private Queue<Drink> drinks = new LinkedList<>();
+
+}
