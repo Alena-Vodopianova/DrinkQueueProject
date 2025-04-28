dee2fc4bbf5fc88ef6a7764894d8c294b6319b14
Alena <alena.vodopianova@edu.fh-joanneum.at>
2025-04-28 19:22:54 +0200

    Added JavaDoc

--- src/main/java/at/fhj/msd/DrinkQueue.java
+++ src/main/java/at/fhj/msd/DrinkQueue.java
@@ -8,38 +8,22 @@ import java.util.Queue;
  * A queue to store and manage Drink objects.
  */
 public class DrinkQueue implements IQueue {
+
     /**
      * The underlying queue to store Drink objects.
      */
     private Queue<Drink> drinks = new LinkedList<>();
 
-    /**
-     * Adds the specified Drink to the queue.
-     *
-     * @param drink The Drink to add
-     * @return true if the Drink was added successfully
-     */
     @Override
     public boolean offer(Drink drink) {
         return drinks.offer(drink);
     }
 
-    /**
-     * Retrieves and removes the first Drink from the queue.
-     *
-     * @return The first Drink in the queue, or null if the queue is empty
-     */
     @Override
     public Drink poll() {
         return drinks.poll();
     }
 
-    /**
-     * Retrieves and removes the first Drink from the queue, throwing an exception if the queue is empty.
-     *
-     * @return The first Drink in the queue
-     * @throws java.util.NoSuchElementException if the queue is empty
-     */
     @Override
     public Drink remove() {
         Drink drink = drinks.poll();
@@ -49,22 +33,11 @@ public class DrinkQueue implements IQueue {
         return drink;
     }
 
-    /**
-     * Retrieves the first Drink in the queue without removing it.
-     *
-     * @return The first Drink in the queue, or null if the queue is empty
-     */
     @Override
     public Drink peek() {
         return drinks.peek();
     }
 
-    /**
-     * Retrieves the first Drink in the queue without removing it, throwing an exception if the queue is empty.
-     *
-     * @return The first Drink in the queue
-     * @throws java.util.NoSuchElementException if the queue is empty
-     */
     @Override
     public Drink element() {
         Drink drink = drinks.peek();
@@ -74,11 +47,6 @@ public class DrinkQueue implements IQueue {
         return drink;
     }
 
-    /**
-     * Returns the number of Drinks in the queue.
-     *
-     * @return The size of the queue
-     */
     public int size() {
         return drinks.size();
     }
